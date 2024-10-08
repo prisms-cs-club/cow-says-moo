2ab4da13d0740e5a0341d897c72851088342e205
HackingHackers <52874216+HackingHackers@users.noreply.github.com>
2024-10-07 22:19:53 -0400

    Add counter

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	let count = 0;
+
+	function increment() {
+		count++;
+	}
+</script>
+
+<div class="py-6">
+	<span class="countdown font-mono text-6xl">
+		<span style="--value:{count};"></span>
+	</span>
+</div>
+<button class="btn btn-primary" on:click={increment}>Increase count</button>
---
+++ src/routes/hello/[name]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Counter from '$lib/Counter.svelte';
+
+	export let data;
+</script>
+
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+			<h1 class="text-5xl font-bold">Hello {data.name}!</h1>
+			<p class="py-6">
+				Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+				quasi. In deleniti eaque aut repudiandae et a id nisi.
+			</p>
+			<Counter />
+		</div>
+	</div>
+</div>
