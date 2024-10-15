b5fb472d248a33f19c6639c38587509e7bae5621
Yikyeung Zhao <fAaAtDoOoG@gmail.com>
2024-10-15 15:58:34 -0400

    added some 114514 elements into the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import logo from '$lib/assets/114514.jpeg';
+	var yeshouxianbei = 114514;
+	function increment() {
+		yeshouxianbei += 1;
+	}
+</script>
+
+<h1>1919810</h1>
+<button on:click={increment}>
+	We love {yeshouxianbei}!
+	<img src={logo} alt="" />
+</button>
+.
+
+<style>
+	h1 {
+		color: yellow;
+		font-family: cursive;
+		font-size: 4em;
+	}
+	button {
+		color: blue;
+		font-family: cursive;
+		font-size: 2em;
+	}
+	button:hover {
+		color: red;
+	}
+</style>
