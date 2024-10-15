b31a4a2509a5301b4d664af00dc18751e253eea2
Jammy <jammys@126.com>
2024-10-15 15:51:22 -0400

    Added a timer that does not work yet

--- src/routes/hello/+page.svelte
+++ src/routes/hello/+page.svelte
@@ -1,7 +1,11 @@
 <h1>Hello</h1>
 <p>This is Jammy's first HTML code</p>
 <b>Really it's the first code!</b>
-<p></p>
+<script>
+let counter = 1;
+</script>
+<p>How many seconds you've been on this page: {counter}</p>
+<p>NOTE: The timer does not work yet</p>
 <i>Wait... We want some of your suggestions!</i>
 <p></p>
 <button class = "btn">Click if you agree that this code is awesome</button>
