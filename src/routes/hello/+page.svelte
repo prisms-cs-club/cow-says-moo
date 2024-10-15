9c286b9eae4ed1f324854740d593c508e2cc5783
Jammy <jammys@126.com>
2024-10-15 15:48:19 -0400

    Added some cool buttons

---
+++ src/routes/hello/+page.svelte
@@ -0,0 +1,8 @@
+<h1>Hello</h1>
+<p>This is Jammy's first HTML code</p>
+<b>Really it's the first code!</b>
+<p></p>
+<i>Wait... We want some of your suggestions!</i>
+<p></p>
+<button class = "btn">Click if you agree that this code is awesome</button>
+<button class = "btn">Click if you agree that this code is NOT awesome</button>
