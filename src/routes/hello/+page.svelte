784838a63fdd6bcbd5fbe3566c0ffa6232bf9fc9
Jammy <jammys@126.com>
2024-10-16 08:55:18 -0400

    Moo

--- src/routes/hello/+page.svelte
+++ src/routes/hello/+page.svelte
@@ -1,12 +1,19 @@
+
 <h1>Hello</h1>
 <p>This is Jammy's first HTML code</p>
 <b>Really it's the first code!</b>
 <script>
+
 let counter = 1+1;
 </script>
 <p>What is 1 + 1: {counter}</p>
 <p>This page is awesome</p>
 <i>Wait... We want some of your suggestions!</i>
-<p></p>
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(__) &nbsp;&nbsp;&nbsp;Moo</p>
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(oo) </p>
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/------\/ </p>
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;||</p>
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*&nbsp;&nbsp;/\----/\</p>
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;~~&nbsp;&nbsp;&nbsp;~~&nbsp;</p>
 <button class = "btn">Click if you agree that this code is awesome</button>
 <button class = "btn">Click if you agree that this code is NOT awesome</button>
