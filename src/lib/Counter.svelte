cd7c3e1de4ad0d645fb579a0e53cac3a27b91b4a
ss77ii <ziruiwang.spencer@gmail.com>
2024-10-16 10:00:02 -0400

    svelte learning

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,8 @@
+<script>
+	var num = 0;
+	function onclick() {
+		num++;
+	}
+</script>
+
+<button class="btn" on:click={onclick} style="display: block;"> Heheheha {num}</button>
---
+++ src/routes/hello/[name]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Counter from '$lib/Counter.svelte';
+	export let data;
+</script>
+
+<!-- center aligned -->
+<h1 class="text-center">Hi {data.name}</h1>
+
+<p>This is a paragraph</p>
+<p>This is a paragraph with <strong>bold</strong> and <em>italic</em> text</p>
+
+<Counter />
+<Counter />
