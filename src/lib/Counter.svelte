93732558638e40a8e32f6d167a6ef15e5ae818b8
IveyHanyuWang <ivey.wang@prismsus.org>
2024-11-01 15:28:52 -0400

    Finish exercise

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,7 @@
+<script>
+    let counter = 0;
+</script>
+<h1 align='center'><button class='btn btn-primary' on:click={() => counter++}>Increase count</button></h1>
+<span class="countdown font-mono text-6xl">
+    <span style="--value:${counter};"></span>
+</span>
--- src/routes/hello/+page.svelte
+++ src/routes/hello/+page.svelte
@@ -1,1 +1,16 @@
-<p>This is under hello/ subpath</p>
+<script>
+	import Counter from "$lib/Counter.svelte";
+</script>
+
+<h1 align="center"><b>Hello Tom!</b></h1>
+<h2 align="center">This is a paragraph.</h2>
+<h2 align="center">This is a paragraph with <b>bold</b> and <i>italic</i>texts.</h2>
+<Counter />
+
+<style>
+    h1 {
+        font-size: 50px;
+    }
+
+
+</style>
