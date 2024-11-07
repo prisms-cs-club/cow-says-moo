5cd70dc0c20efdbe493ff0e1670a2cea3af4f24f
Jammy <jammys@126.com>
2024-11-07 15:57:02 -0500

    Includes PIN, a new website project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>PIN Homepage</h1>
+<p>Welcome to the PRISMS International News homepage. You will be able to see information about house events.</p>
+
+<p>Latest news:</p>
+<a href="/test">Comment test</a>
--- src/routes/hello/+page.svelte
+++ src/routes/hello/+page.svelte
@@ -1,15 +1,15 @@
-<script>
-	import Counter from '$lib/potato_counter.svelte';
-	import CarrotCounter from '$lib/carrot_counter.svelte';
+<script lang="ts">
+	import Counter from '$lib/counter.svelte';
 </script>
 
 <h1>Hello</h1>
 <p>This is Jammy's first HTML code</p>
 <b>Really it's the first code!</b>
-<Counter />
-<CarrotCounter />
+<Counter logo="🥔" message="How much money have you spent on potatoes: $" />
+<Counter logo="🥕" message="How much money have you spent on carrots: $" />
+<Counter logo="🪙" message="How much coins have you collected on the sidewalk: ¢" />
 <p>This page is awesome</p>
-<i>Wait... We want some of your suggestions!</i>
+<i>Try adding a name at the end of the address! It will surely surprise you!</i>
 <p>
 	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(__)
 	&nbsp;&nbsp;&nbsp;Moo
@@ -25,5 +25,3 @@
 <p>
 	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;~~&nbsp;&nbsp;&nbsp;~~&nbsp;
 </p>
-<button class="btn">Click if you agree that this code is awesome</button>
-<button class="btn">Click if you agree that this code is NOT awesome</button>
