1bf86410f88515b7e4e70cd7c45056d25e859037
asteen24 <118776001+asteen24@users.noreply.github.com>
2024-11-07 15:58:53 -0500

    nav bar

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,6 @@
+
+<script>
+    let counter = 1;
+</script>
+<p> Value of the counter: {counter}</p>
+<button class = "btn" on:click = {() => counter++}> Click me! </button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<h1> hello</h1>>
-<p> hello </p>
-<b> hello</b>
-<i>h</i>
-<button class = "btn" > Click me! </button>
+<html>
+  <head>
+    <title>Overview</title>
+    <link href="Overview.css" rel="stylesheet" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  
+<!--Nav Bar</-->
+  <body>
+    <div id="wrapper">
+      
+      <div class="parallax"></div>
+      
+      <div id="navbar">
+        <ul>
+          <li class="o"> <a href="index.html">Home</a></li>
+          <li class="o"> <a href="index.html">House Rankings</a></li>
+          <li class="o"> <a href="index.html">Upcoming Events</a></li>
+          <li class="o"> <a href="index.html">Calendar</a></li>
+          <li class="o"> <a href="index.html">Activity Suggestions</a></li>
+
+        </ul>
+      </div> 
+      
+    </div>
+  </body>
+</html>
