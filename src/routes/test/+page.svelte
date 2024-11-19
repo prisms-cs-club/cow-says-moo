a1b8a0722d16e1c8bd654784cc7e01d81f5adf70
Jammy <jammys@126.com>
2024-11-19 15:44:01 -0500

    Changes potentially politically-dangerous content

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -4,6 +4,6 @@
 "title": "Ettl did great on the frisbee tournament",
 "author": "Jammy",
 "date": "11/19/24",
-"content": "Ettl is excellent! Ettl excels! 稍有常识的人都能看出，如果我们的队伍继续前行，那些小小的困难还能阻挡的了我们吗?"}`;
+"content": "Ettl is excellent! Ettl excels! Ettl wins awards en masse! Ettl is the Everwin!"}`;
 </script>
 <Renderpage page={test_json_str}/>
