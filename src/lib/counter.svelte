71eb1a845933bbee4fb1483348f76d6e37689b1c
Jammy <jammys@126.com>
2024-11-19 15:40:58 -0500

    Makes the announcement page working by setting a variable in JS instead of using the JSON directly

---
+++ src/lib/counter.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let logo;
+
+	export let message;
+	let counter = 0;
+</script>
+
+<p>{message}{counter}</p>
+
+<button
+	class="btn"
+	on:click={() => {
+		counter++;
+        var audio = new Audio('src/buy.mp3');
+		audio.play();
+	}}>Buy {logo}</button
+>
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import Renderpage from "$lib/renderpage.svelte";
+    let test_json_str = `{
+"title": "Ettl did great on the frisbee tournament",
+"author": "Jammy",
+"date": "11/19/24",
+"content": "Ettl is excellent! Ettl excels! 稍有常识的人都能看出，如果我们的队伍继续前行，那些小小的困难还能阻挡的了我们吗?"}`;
+</script>
+<Renderpage page={test_json_str}/>
