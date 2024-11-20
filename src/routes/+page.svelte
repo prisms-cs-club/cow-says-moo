5c576ea13cac82cdcbda171bfd6f276751b004d9
MqCreaple <gmq14159@gmail.com>
2024-11-20 08:49:10 -0500

    clean up workspace and format code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <h1>PIN Homepage</h1>
-<p>Welcome to the PRISMS International News homepage. You will be able to see information about house events.</p>
+<p>
+	Welcome to the PRISMS International News homepage. You will be able to see information about house
+	events.
+</p>
 
 <p>Latest news:</p>
 <a href="/test">Comment test</a>
--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,9 +1,12 @@
+<!-- Author: Jammy -->
+
 <script lang="ts">
-    import Renderpage from "$lib/renderpage.svelte";
-    let test_json_str = `{
+	import RenderComment from '$lib/RenderComment.svelte';
+	let test_json_str = `{
 "title": "Ettl did great on the frisbee tournament",
 "author": "Jammy",
 "date": "11/19/24",
 "content": "Ettl is excellent! Ettl excels! Ettl wins awards en masse! Ettl is the Everwin!"}`;
 </script>
-<Renderpage page={test_json_str}/>
+
+<RenderComment page={test_json_str} />
