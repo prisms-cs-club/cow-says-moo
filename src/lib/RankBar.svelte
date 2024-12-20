400f9a9915a0b1fde099b2bc7ab4cd25987242c7
nyctivoe <168@nyctivoe.com>
2024-12-20 16:28:41 -0500

    update rankBar

--- src/lib/RankBar.svelte
+++ src/lib/RankBar.svelte
@@ -33,35 +33,110 @@
 
 <h1>House Points</h1>
 {#if shown}
-	<p>
-		<progress class="progress progress-success w-56" value={ettl.points} max={maxPoints}></progress>
-	</p>
-	<p>
-		<progress class="progress progress-error w-56" value={hobler.points} max={maxPoints}></progress>
-	</p>
-	<p>
-		<progress class="progress progress-warning w-56" value={albemarle.points} max={maxPoints}
+	<div class="na">
+		<p class="text">Ettl</p>
+		<progress class="progress-ettl progress w-56" value={ettl.points} max={maxPoints}></progress>
+	</div>
+
+	<div class="na">
+		<p class="text">Hobler</p>
+		<progress class="progress-hobler progress w-56" value={hobler.points} max={maxPoints}
 		></progress>
-	</p>
-	<p>
-		<progress class="progress progress-info w-56" value={lambert.points} max={maxPoints}></progress>
-	</p>
+	</div>
+
+	<div class="na">
+		<p class="text">Albemarle</p>
+		<progress class="progress-albemarle progress w-56" value={albemarle.points} max={maxPoints}
+		></progress>
+	</div>
+
+	<div class="na">
+		<p class="text">Lambert</p>
+		<progress class="progress-lambert progress w-56" value={lambert.points} max={maxPoints}
+		></progress>
+	</div>
 {:else}
 	<span class="loading loading-bars loading-lg"></span>
 {/if}
 
 <style>
+	.text {
+		top: 30px;
+		position: relative;
+		left: 5px;
+	}
 	.progress {
 		position: relative;
 		width: 80%;
 		left: 10%;
-		appearance: none;
-		overflow: hidden;
-		height: 2.5rem;
+		height: 2rem /* 8px */;
 		border-radius: var(--rounded-box, 1rem);
-		background-color: var(--fallback-bc, oklch(var(--bc) / 0.1));
+		background-color: var(--fallback-bc, oklch(var(--bc) / 0.2));
 	}
-	.progress-success::-moz-progress-bar {
-		background-color: var(--fallback-su, oklch(var(--su) / 1));
+	.progress::-moz-progress-bar {
+		border-radius: var(--rounded-box, 1rem /* 16px */);
+		--tw-bg-opacity: 1;
+		background-color: var(--fallback-bc, oklch(var(--bc) / var(--tw-bg-opacity)));
+	}
+	:root {
+		--color-albemarle: #ede625;
+		--color-ettl: #26be3a;
+		--color-hobler: #fa4242;
+		--color-lambert: #4270fa;
+	}
+	.progress-ettl::-moz-progress-bar {
+		border-radius: var(--rounded-box, 1rem);
+		--tw-bg-opacity: 1;
+		background-color: var(--color-ettl);
+	}
+	.progress-ettl:indeterminate {
+		--progress-color: var(--color-ettl);
+	}
+	.progress-ettl::-webkit-progress-value {
+		--tw-bg-opacity: 1;
+		background-color: var(--color-ettl);
+	}
+
+	.progress-albemarle::-moz-progress-bar {
+		border-radius: var(--rounded-box, 1rem);
+		--tw-bg-opacity: 1;
+		background-color: var(--color-albemarle);
+	}
+	.progress-albemarle:indeterminate {
+		--progress-color: var(--color-albemarle);
+	}
+	.progress-albemarle::-webkit-progress-value {
+		--tw-bg-opacity: 1;
+		background-color: var(--color-albemarle);
+	}
+	.progress-lambert::-moz-progress-bar {
+		border-radius: var(--rounded-box, 1rem);
+		--tw-bg-opacity: 1;
+		background-color: var(--color-lambert);
+	}
+	.progress-lambert:indeterminate {
+		--progress-color: var(--color-lambert);
+	}
+	.progress-lambert::-webkit-progress-value {
+		--tw-bg-opacity: 1;
+		background-color: var(--color-lambert);
+	}
+	.progress-hobler::-moz-progress-bar {
+		border-radius: var(--rounded-box, 1rem);
+		--tw-bg-opacity: 1;
+		background-color: var(--color-hobler);
+	}
+	.progress-hobler:indeterminate {
+		--progress-color: var(--color-hobler);
+	}
+	.progress-hobler::-webkit-progress-value {
+		--tw-bg-opacity: 1;
+		background-color: var(--color-hobler);
+	}
+	.loading-bars {
+		display: block;
+		width: 80px;
+		height: 80px;
+		position: relative;
 	}
 </style>
