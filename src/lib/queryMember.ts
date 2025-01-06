8e62242968a0bb5889c4d06418257e74ca67fdcc
nyctivoe <168@nyctivoe.com>
2025-01-06 11:29:37 -0500

    update description for getMember

--- src/lib/queryMember.ts
+++ src/lib/queryMember.ts
@@ -1,6 +1,9 @@
 import type { Member } from "./format";
 import { page } from '$app/state';
 
+/*
+returns either a member, or undefined.
+*/
 export async function getMember() {
     if(page.data.session && page.data.session.user) {
         let member: Member | null = await (await fetch("/api/member?email=" + page.data.session.user.email)).json();
