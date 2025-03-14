d66414af982c8f433d583a6aa80b3ca86b217833
nyctivoe <168@nyctivoe.com>
2025-03-14 16:01:35 -0400

    updates

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,10 +4,10 @@ import Google from "@auth/core/providers/google";
 export const { handle, signIn, signOut } = SvelteKitAuth({
   providers: [Google({
     clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
-    clientSecret: import.meta.env.VITE_GOOGLE_CLIENT_SECRET,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     authorization: 'https://accounts.google.com/o/oauth2/auth?response_type=code&hd=prismsus.org',
   })],
   // You can customize other options here, for example:
-  secret: import.meta.env.VITE_AUTH_SECRET,
+  secret: process.env.AUTH_SECRET,
   trustHost: true,
 });
